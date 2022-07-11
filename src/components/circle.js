493eaf8dec96cc8c554316c60864924284cdb6df
Sauvik Paria <97239298+IMsauvik@users.noreply.github.com>
2022-07-11 21:59:51 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,13 @@
-// import using require
-
 // declare class
 
+class Shape {
+  constructor() {
+    this.color = "red";
+  }
+  drawShape() {}
 
+  calculateArea() {}
+}
+
+module.exports = Shape;
 // export class using module.exports
